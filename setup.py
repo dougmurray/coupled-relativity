8699787c2bcb2fbea412fd8c7c1dffd2dbfebaad
Doug Murray <murray@twinleaf.com>
2017-08-22 11:20:40 -0400

    Added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='coupled-relativity',
+      version='0.1',
+      description='Module for making data analysis in coupled realativitic wave dynamics',
+      url='http://github.com/dougmurray/coupled-relativity',
+      author='Doug Murray',
+      author_email='douglass.murray@gmail.com',
+      license='MIT',
+      packages=['coupled-relativity'],
+      zip_safe=False)
